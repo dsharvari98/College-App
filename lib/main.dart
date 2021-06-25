dfd85ed32a685834d3848b79f2bc5d28f6995692
poojakatpale4588 <70963338+poojakatpale4588@users.noreply.github.com>
2021-06-25 20:25:17 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:school_management/calendar.dart';
+import 'package:school_management/home.dart';
+
+void main() {
+  runApp(SchoolManagement());
+}
+
+class SchoolManagement extends StatefulWidget {
+  @override
+  _SchoolManagementState createState() => _SchoolManagementState();
+}
+
+class _SchoolManagementState extends State<SchoolManagement> {
+  int _selectedItemIndex = 0;
+  final List pages = [
+    HomePage(),
+    null,
+    null,
+    CalendarPage(),
+    null,
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 0,
+          backgroundColor: Color(0xFFF0F0F0),
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.black,
+          selectedIconTheme: IconThemeData(color: Colors.blueGrey[600]),
+          currentIndex: _selectedItemIndex,
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) {
+            setState(() {
+              _selectedItemIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              label: '',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Icon(Icons.insert_chart),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Icon(Icons.done),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Icon(Icons.calendar_today),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Icon(Icons.chat_bubble),
+            ),
+          ],
+        ),
+        body: pages[_selectedItemIndex]
+      ),
+    );
+  }
+}
